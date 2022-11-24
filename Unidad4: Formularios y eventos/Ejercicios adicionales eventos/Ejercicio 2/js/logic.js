8685e74c705639c869e4aecb2b41a76e11b2b2d2
ClaudiaYLopezLafita <claudiayll94@gmail.com>
2022-11-24 20:47:10 +0100

    Ejercicio 2 - evento adicional

---
+++ Unidad4: Formularios y eventos/Ejercicios adicionales eventos/Ejercicio 2/js/logic.js
@@ -0,0 +1,58 @@
+
+/* 1. CLICK CAMBIA EL BACKGROUD-COLOR */
+
+document.addEventListener("click", clickChangeDiv,false);
+/**
+ * le damos estilo a un elemento cuando se clickea
+ */
+function clickChangeDiv(){
+    console.log('Entrando en la funcion myFunction sin parámetro');
+    //al elemento con id container le damos unos atributos determinados
+    document.getElementById('container').style.cssText = 'padding:20px;' +
+                                                        'background-color:#D4AC0D;'+ 
+                                                        'color:#0c0800;'+ 
+                                                        'border:1px solid #0c0800;'+ 
+                                                        'font-size:22px;';
+    console.log('Saliendo de la funcion myFunction sin parámetro');
+}
+
+/* 2. CUANDO PULSAMOS UNA TECLA */
+
+document.addEventListener('keydown', keydownChangeBody,false);
+
+function keydownChangeBody(){
+    console.log('Entrando en la funcion keydownChangeBody')
+    document.getElementById("body").style.backgroundColor = "#85C1E9";
+}
+
+/** 3. MOVER EL RATON */
+
+document.addEventListener('mousemove', mouseMoveChageBody,false);
+
+function mouseMoveChageBody(){
+    document.getElementById("body").style.backgroundColor = "#FDFEFE"
+}
+
+/** 4. CLICK EL RATON */
+
+document.addEventListener('mousedown',clickMouseCoordenadas,false);
+
+function clickMouseCoordenadas(e){
+    var x = e.clientX;
+	var y = e.clientY;
+    document.getElementById('result').innerHTML = `Las coordenadas del raton [${x} - ${y}]`;
+    // document.getElementById('result2').innerHTML =`\nLas coordenadas del ratón respento al navegador: ${coordX} - ${coordY}`;
+}
+
+/** 5. CUANDO PULSAMOS UNA TECLA MOSTRAR INFORMACION */
+
+document.addEventListener('keydown', keyDonwMostrarInfo, false);
+
+function keyDonwMostrarInfo(e){
+
+    var mensaje = "Propiedad keyCode: " + e.keyCode + "<br>" +
+    "Propiedad charCode: " + e.charCode + "<br>" +
+    "Valor ascii: " + String.fromCharCode(e.charCode);
+
+    document.getElementById('result3').innerHTML = mensaje
+}
